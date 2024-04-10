075a30555974b0a89efbe943d2b74be8d9ecac1f
ttedgar <150164985+ttedgar@users.noreply.github.com>
2024-04-10 13:46:37 +0200

    navbar added to admin

--- client/my-react-app/src/components/Admin.jsx
+++ client/my-react-app/src/components/Admin.jsx
@@ -1,9 +1,12 @@
 import NewPlanetForm from './NewPlanetfForm'
 import AdminDestinations from './AdminDestinations'
+import NavBar from './NavBar'
 
 function Admin() {
   return (
+
     <div>
+      <NavBar/>
       <h1>Admin page</h1>
       <div><NewPlanetForm/></div>
       <div><AdminDestinations/></div>
