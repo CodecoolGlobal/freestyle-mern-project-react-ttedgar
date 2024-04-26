752180bf726d9b214a9d9e8f8ddce08523bd761b
ttedgar <150164985+ttedgar@users.noreply.github.com>
2024-04-26 08:39:24 +0200

    starmap navigation

--- client/my-react-app/src/page/Starmap.jsx
+++ client/my-react-app/src/page/Starmap.jsx
@@ -1,18 +1,28 @@
 import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   const characterRef = useRef();
   const [top, setTop] = useState(-1250)
   const [left, setLeft] = useState(1)
+  const navigate = useNavigate();
 
   useEffect(() => {
     characterRef.current.style.top = `-1250px`;
     characterRef.current.style.left = `5px`;
   }, [])
 
+  function Coruscant() {
+    if (top < -700 && top > -800 && left > 550 && left < 650) {
+      const coruscant = window.confirm('You want to go to Coruscant?')
+      coruscant ? navigate('/destinations/Coruscant') : null;
+    }
+  }
+
   useEffect(() => {
     function handleKeyPress(event) {
       if (event.key === '2' && top < -100) {
+        Coruscant()
         console.log(top);
         setTop((prevPosition) => {
           const newPosition = prevPosition + 50;
@@ -21,6 +31,7 @@ function App() {
           return newPosition;
         });
       } else if (event.key === '5' && top > -1250) {
+        Coruscant()
         console.log(top);
         setTop((prevPosition) => {
           const newPosition = prevPosition - 50;
@@ -29,6 +40,7 @@ function App() {
           return newPosition;
         });
       } else if (event.key === '3' && left < 1300) {
+        Coruscant()
         console.log(left);
         setLeft((prevPosition) => {
           const newPosition = prevPosition + 50;
@@ -37,6 +49,7 @@ function App() {
           return newPosition;
         });
       } else if (event.key === '1' && left > 50) {
+        Coruscant()
         setLeft((prevPosition) => {
           console.log(left);
           const newPosition = prevPosition - 50;
